561a65a97cecf70800d11493481b2effa17a88df
jacqueline ngabo <ngabo.jay@gmail.com>
2022-03-05 01:32:04 +0300

    add script.js changes

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+// var akan = function (year,month,date){
+
+//     var olddate = new Date(year,--month,day);
+// var year = useryear;
+// var month = usermonth;
+// var date = userDate;
+
+
+// }
+// var useryear = Number(prompt('Enter year of birth'));
+// var userdate = Number(prompt('Enter date of birth'));
+// var usermonth = Number(prompt('enter month of birth'));
+// alert(akan);
+
+document.getElementById('date')
+
+
+
+
+
+
+
