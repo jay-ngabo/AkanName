bb8f189b7eea6939c3cdf7e5ab15a71bba31d677
jacqueline ngabo <ngabo.jay@gmail.com>
2022-03-06 15:05:16 +0300

    add current javascript chnages

--- js/script.js
+++ js/script.js
@@ -10,10 +10,12 @@ submit.addEventListener('click', ()=>{
 }
 )
 function calculate(){
-    var cc = year. value.slice(0,2);
-    var yy = year.value.slice(2,3);
-    var date = date.value;
-    var month = month.value;
+    var CC = year. value.slice(0,2);
+    var YY = year.value.slice(2,3);
+    var DD = date.value;
+    var MM = month.value;
+
+    (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
 
 
 
