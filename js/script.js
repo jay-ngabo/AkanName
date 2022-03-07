0d154560f56fe240766be0fb32a51e3bf60f2217
jacqueline ngabo <ngabo.jay@gmail.com>
2022-03-07 12:56:47 +0300

    add changes in javascript file

--- js/script.js
+++ js/script.js
@@ -1,78 +1,44 @@
+function formRefresh(){
+    document.getElementById("form_data").reset();
+}
 
-//  var submit = document.getElementById('submit');
+// Event function on button
+function akanNames() {
 
-// submit.addEventListener('click',(event)=>{
-//     event.preventDefault();
-//      var month = document.getElementById('month').value;
-// console.log(month);
-// var date = document.getElementById('date').value;
-// console.log(date);
-// var year = document.getElementById('year').value;
-// console.log(year);
-// })
+    let birthDate = document.getElementById("birthdate").value;
+    let dateEntered = birthDate.split("-");
+    let day = parseInt(dateEntered[2]);
+    let month = parseInt(dateEntered[1]);
+    let year = parseInt(dateEntered[0]);
 
-// var submit = addEventListener ('click', function (event){
-//     event.preventDefault();
-//     console.log(submit)
-//         let month = document.getElementById('month').value;
-//         console.log(month)
-//         let date = document.getElementById('date').value;
-//         console.log(date)
-//         let year = document.getElementById('year').value;
-//         console.log(year)
-//     let gender = document.getElementById('gender').value;
-//         console.log(gender);
+    //Centuary calculation
+    let centry = (year - 1) / 100 + 1;
 
-//         let calculate; {
-//             var CC = year.slice(0,2);
-//             console.log(CC);
-//             var YY = year.slice(2,4);
-//             console.log(YY);
-//             var DD = date;
-//             console.log(DD);
-//             var MM = month;
-//             console.log(MM);
-//             (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
-//             console.log(d);
-            
-//             // return calculate;
-//         }
-//         if (date < 1 || date >31){
-// alert('invalid')};
-// if (month.value < 0 || date.value > 12) {
+    let dayOfTheWeek = (centry / 4 - 2 * centry - 1 + 5 * year / 4 + 26 * (month + 1) / 10 + day) % 7;
 
-//     alert("invalid Date of Birth! ")
-// }
-let akanMale =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
-let akanFemale =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
-let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-let button = document.getElementById("btn");
-// alert("OK")
-let form = document.getElementById("form");
-form.addEventListener("submit", (e) =>{
-    e.preventDefault();
-    // date constructor
-    let formData = new FormData(e.target);
-    let myDate = new Date(formData.get("name"));
-    let birthDay = myDate.getDay();
-    alert(birthDay)
-    // gender fuctionality
-    var gender = document.getElementsByName("gender");
-    for (var i = 0; i < gender.length; i++) {
-        if (gender[i].checked)
-            var genderValue = gender[i].value;
+    document.getElementById("display").innerHTML = Math.round(dayOfTheWeek);
+    let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    let maleNames = ["Kwasi", " Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    let gender = document.getElementById("gender").value;
+    let daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let dOfWeek = Math.round(dayOfTheWeek)
+
+    //form validation and output
+    if (!isNaN(dOfWeek)) {
+        if (gender == "Male") {
+            document.getElementById("display").innerHTML = "Your akan names is " + maleNames[Math.round(dayOfTheWeek)] + " and you were born on " + daysOfWeek[dOfWeek];
+        } else if (gender === "Female") {
+            document.getElementById("display").innerHTML = "You were born on " + daysOfWeek[dOfWeek] + " and Your akan name is " + femaleNames[Math.round(dayOfTheWeek)];
+        } else {
+            alert("Enter valid dates and choose your gender");
+            document.getElementById("display").innerHTML = "";
+        }
+    } else {
+        alert("Enter valid birthday and choose your gender");
+        document.getElementById("display").innerHTML = "";
     }
-    // gender pick statements
-    if (genderValue === "female"){
-        document.getElementById("congrats").innerHTML = ("HURRAY");
-        document.getElementById("congrats").style.color = "#CCD6F6"
-        document.getElementById("second").innerHTML = (" You were born on" +days[birthDay]+ "And your Akan name is" +akanFemale[birthDay]);
-        alert(akanFemale)
-    }
-    else{
-        document.getElementById("congrats").innerHTML = ("HURRAY");
-        document.getElementById("congrats").style.color = "#CCD6F6"
-        document.getElementById("second").innerHTML = (" You were born on" +days[birthDay]+ "And your Akan name is" +akanMale[birthDay]);
-        alert(akanMale)
-    }
-})
+
+
+    //call to the reFresh function
+    formRefresh();
+}
